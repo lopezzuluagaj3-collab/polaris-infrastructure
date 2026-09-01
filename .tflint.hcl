b0b69d6184216897f55cb7f1256489b0a6018074
lopezzuluagaj3-collab <lopezzuluagaj3@gmail.com>
2026-09-01 17:47:40 -0500

    add: se agrega una nueva herramienta para el ci/cd tflint

---
+++ .tflint.hcl
@@ -0,0 +1,10 @@
+config {
+  call_module_type = "local"
+  force            = false
+}
+
+plugin "aws" {
+  enabled = true
+  version = "0.38.0"
+  source  = "github.com/terraform-linters/tflint-ruleset-aws"
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5.0" 
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -14,7 +16,7 @@ terraform {
   }
 }
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 data "aws_availability_zones" "available" {}
